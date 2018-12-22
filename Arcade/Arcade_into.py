957463e8806628314bf66e8a6382eb3f9694e6da
khaldon <mohamed.khaled33388@gmail.com>
2018-12-22 21:43:02 +0200

    Intro

---
+++ Arcade/Arcade_into.py
@@ -0,0 +1,40 @@
+# Intro 
+
+# The Journey Begins
+
+# Add challange 
+def add(param1, param2):
+    return param1 + param2
+print(add(1,2))
+
+# centuryFromYear challange
+def centuryFromYear(year):
+    year = year/100
+    if year == int(year):
+        return year
+    else:
+        return int(year)+1
+            
+print(centuryFromYear(1905))
+
+
+# palindrome challange 
+def checkPalindrome(inputString):
+    if inputString[::-1] == inputString:
+        return True
+    else:
+        return False
+
+# Edge of the Ocean
+
+# adjacentElementsProduct challange 
+def adjacentElementsProduct(inputArray):
+	store_result = []
+	length = len(inputArray)
+	length_after = length-1
+	for i in range(length_after):
+		store_result.append(inputArray[i]*inputArray[length-len(inputArray[i:length_after])])
+	return max(store_result)
+
+adjacentElementsProduct([3, 6, -2, -5, 7, 3])
+
